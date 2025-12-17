fe82f87904d3ce982471f460b009f325fdb36260
jka-kan <janne.kankaala@gmail.com>
2025-12-17 13:34:55 +0100

    new exercises added

--- editor/edit.py
+++ editor/edit.py
@@ -166,14 +166,14 @@ alt_ending_fingering_R = []
 # amount = 14
 
 # 8
-pattern_up = ["C2", "E2", "G2", "A2", "F2", "G2", "E2", "F2"]
-pattern_down = ["G4", "E4", "C4", "B3", "D4", "C4", "E4", "D4"]
-
-finger_up_R = [1, 2, 4, 5, 3, 4, 2, 3]
-finger_down_R = [5, 4, 2, 1, 3, 2, 4, 3]
-
-finger_up_L = [5, 4, 2, 1, 3, 2, 4, 3]
-finger_down_L = [1, 2, 4, 5, 3, 4, 2, 3]
+# pattern_up = ["C2", "E2", "G2", "A2", "F2", "G2", "E2", "F2"]
+# pattern_down = ["G4", "E4", "C4", "B3", "D4", "C4", "E4", "D4"]
+#
+# finger_up_R = [1, 2, 4, 5, 3, 4, 2, 3]
+# finger_down_R = [5, 4, 2, 1, 3, 2, 4, 3]
+#
+# finger_up_L = [5, 4, 2, 1, 3, 2, 4, 3]
+# finger_down_L = [1, 2, 4, 5, 3, 4, 2, 3]
 
 # alt_transition_L = ["B3", "D4", "C4", "E4", "D4", "F4", "E4", "D4"]
 # alt_transition_R = ["B4", "D5", "C5", "E5", "D5", "F5", "E5", "D5"]
@@ -185,7 +185,88 @@ finger_down_L = [1, 2, 4, 5, 3, 4, 2, 3]
 # alt_ending_fingering_R = [5, 3, 4, 2, 3, 1, 3, 4]
 # alt_ending_fingering_L = [1, 3, 2, 4, 3, 5, 3, 2]
 
-amount = 14
+# amount = 14
+
+# 9
+# pattern_up = ["C2", "E2", "F2", "E2", "G2", "F2", "A2", "G2"]
+# pattern_down = ["G4", "E4", "D4", "E4", "C4", "D4", "B3", "C4"]
+#
+# finger_up_R = [1, 2, 3, 2, 4, 3, 5, 4]
+# finger_down_R = [5, 4, 3, 4, 2, 3, 1, 2]
+#
+# finger_up_L = [5, 4, 3, 4, 2, 3, 1, 2]
+# finger_down_L = [1, 2, 3, 2, 4, 3, 5, 4]
+#
+# alt_ending_L = ["A2", "F2", "E2", "F2", "D2", "E2", "D2", "E2"]
+# alt_ending_R = ["A3", "F3", "E3", "F3", "D3", "E3", "D3", "E3"]
+# alt_ending_fingering_R = [5, 3, 4, 2, 2, 3, 2, 3]
+# alt_ending_fingering_L = [1, 2, 3, 2, 4, 5, 4, 5]
+#
+#
+# amount = 14
+
+# 10
+# pattern_up = ["C2", "A2", "G2", "F2", "E2", "F2", "E2", "F2"]
+# pattern_down = ["G4", "B3", "C4", "D4", "E4", "D4", "E4", "D4"]
+#
+# finger_up_R = [1, 5, 4, 3, 2, 3, 2, 3]
+# finger_down_R = [5, 1, 2, 3, 3, 2, 3, 2]
+#
+# finger_up_L = [5, 1, 2, 3, 4, 3, 4, 3]
+# finger_down_L = [1, 5, 2, 3, 2, 3, 2, 3]
+#
+# amount = 14
+
+
+# 11
+# pattern_up = ["C2", "E2", "A2", "G2", "A2", "G2", "F2", "G2"]
+# pattern_down = ["G4", "D4", "B3", "C4", "B3", "C4", "D4", "C4"]
+#
+# finger_up_R = [1, 2, 5, 4, 5, 4, 3, 4]
+# finger_down_R = [5, 2, 1, 2, 1, 2, 3, 2]
+#
+# finger_up_L = [5, 3, 1, 2, 5, 4, 3, 4]
+# finger_down_L = [1, 3, 5, 4, 5, 4, 3, 4]
+
+# amount = 14
+
+# 12
+#### First measure is different, changed manually !!!
+# pattern_up = ["A2", "C2", "E2", "D2", "C2", "D2", "E2", "C2"]
+# pattern_down = ["B3", "G4", "E4", "F4", "G4", "F4", "E4", "G4"]
+#
+# finger_up_R = [5, 1, 3, 2, 1, 2, 3, 1]
+# finger_down_R = [1, 5, 3, 4, 5, 4, 3, 5]
+#
+# finger_up_L = [1, 5, 3, 4, 5, 4, 3, 5]
+# finger_down_L = [5, 1, 3, 2, 1, 2, 3, 1]
+#
+# alt_transition_L = ["G4", "B3", "D4", "C4", "B3", "C4", "D4", "E4"]
+# alt_transition_R = ["G5", "B4", "D5", "C5", "B4", "C5", "D5", "E5"]
+# alt_transition_fingering_R = [5, 1, 3, 2, 1, 2, 3, 4]
+# alt_transition_fingering_L = [1, 5, 2, 3, 5, 4, 3, 2]
+#
+# alt_ending_L = ["C2", "G2", "E2", "F2", "G2", "F2", "E2", "F2"]
+# alt_ending_R = ["C3", "G3", "E3", "F3", "G3", "F3", "E3", "F3"]
+# alt_ending_fingering_R = [1, 5, 3, 4, 5, 4, 3, 4]
+# alt_ending_fingering_L = [5, 1, 3, 2, 1, 2, 3, 2]
+#
+# # Amount logic changed here !!!!
+# amount_up = 14
+# amount_down = 15
+
+# 13
+pattern_up = ["E2", "C2", "F2", "D2", "G2", "E2", "F2", "G2"]
+pattern_down = ["E4", "G4", "D4", "F4", "E4", "C4", "D4", "E4"]
+
+finger_up_R = [3, 1, 4, 2, 5, 3, 4, 5]
+finger_down_R = [3, 5, 2, 4, 3, 1, 3, 4]
+
+finger_up_L = [3, 5, 2, 4, 1, 3, 2, 1]
+finger_down_L = [3, 1, 4, 2, 3, 5, 3, 2]
+
+amount_up = 14
+amount_down = 14
 
 
 def make_sequence(pattern, hand, cycles, finger_pattern, direction):
@@ -203,8 +284,10 @@ def make_sequence(pattern, hand, cycles, finger_pattern, direction):
                 orig_key = key
 
             if direction == "up":
+                print("up")
                 keys_iterator = iter(white_keys.keys())
             else:
+                print("down")
                 keys_iterator = iter(reversed(white_keys.keys()))
             while True:
 
@@ -228,22 +311,24 @@ complete = {"R": [], "L": []}
 # Left
 
 if alt_transition_L:
-    complete["L"] = make_sequence(pattern_up, "L", amount - 1, finger_up_L, "up")
+    complete["L"] = make_sequence(pattern_up, "L", amount_up - 1, finger_up_L, "up")
     for note in alt_transition_L:
         full_note = note + "-16-" + str(alt_transition_fingering_L.pop(0))
         complete["L"].append(full_note)
 else:
-    complete["L"] = make_sequence(pattern_up, "L", amount, finger_up_L, "up")
+    complete["L"] = make_sequence(pattern_up, "L", amount_up, finger_up_L, "up")
 
 if alt_ending_L:
-    for note in make_sequence(pattern_down, "L", amount - 1, finger_down_L, "down"):
+    for note in make_sequence(
+        pattern_down, "L", amount_down - 1, finger_down_L, "down"
+    ):
         complete["L"].append(note)
 
     for note in alt_ending_L:
         full_note = note + "-16-" + str(alt_ending_fingering_L.pop(0))
         complete["L"].append(full_note)
 else:
-    for note in make_sequence(pattern_down, "L", amount, finger_down_L, "down"):
+    for note in make_sequence(pattern_down, "L", amount_down, finger_down_L, "down"):
         complete["L"].append(note)
 
 complete["L"].append("C2-16-5")
@@ -251,21 +336,23 @@ complete["L"].append("C2-16-5")
 
 # Right
 if alt_transition_R:
-    complete["R"] = make_sequence(pattern_up, "R", amount - 1, finger_up_R, "up")
+    complete["R"] = make_sequence(pattern_up, "R", amount_up - 1, finger_up_R, "up")
     for note in alt_transition_R:
         full_note = note + "-16-" + str(alt_transition_fingering_R.pop(0))
         complete["R"].append(full_note)
 else:
-    complete["R"] = make_sequence(pattern_up, "R", amount, finger_up_R, "up")
+    complete["R"] = make_sequence(pattern_up, "R", amount_up, finger_up_R, "up")
 
 if alt_ending_R:
-    for note in make_sequence(pattern_down, "R", amount - 1, finger_down_R, "down"):
+    for note in make_sequence(
+        pattern_down, "R", amount_down - 1, finger_down_R, "down"
+    ):
         complete["R"].append(note)
     for note in alt_ending_R:
         full_note = note + "-16-" + str(alt_ending_fingering_R.pop(0))
         complete["R"].append(full_note)
 else:
-    for note in make_sequence(pattern_down, "R", amount, finger_down_R, "down"):
+    for note in make_sequence(pattern_down, "R", amount_down, finger_down_R, "down"):
         complete["R"].append(note)
 
 complete["R"].append("C3-16-1")
